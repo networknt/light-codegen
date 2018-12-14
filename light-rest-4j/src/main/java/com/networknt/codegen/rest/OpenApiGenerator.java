903d34ed7fc88cd389a51b7c567919055507c7dc
Jiachen Sun <31008528+jiachen1120@users.noreply.github.com>
2018-12-14 10:28:59 -0500

    Add underscores automatically when space are present - closes #182 (#192)
    * Fixed -Add underscores automatically when space are present #182
    * Added test cases to check whether the fields' name contains space #182

--- light-rest-4j/src/main/java/com/networknt/codegen/rest/OpenApiGenerator.java
+++ light-rest-4j/src/main/java/com/networknt/codegen/rest/OpenApiGenerator.java
@@ -195,6 +195,7 @@ public class OpenApiGenerator implements Generator {
                                 //System.out.println("key = " + entryProp.getKey() + " value = " + entryProp.getValue());
                                 Map<String, Any> propMap = new HashMap<>();
                                 String name = entryProp.getKey();
+                                name = name.trim().replaceAll(" ", "_");
                                 propMap.put("jsonProperty", Any.wrap(name));
                                 if(name.startsWith("@")) {
                                     name = name.substring(1);
@@ -241,6 +242,7 @@ public class OpenApiGenerator implements Generator {
                                     if("enum".equals(entryElement.getKey())) {
                                         propMap.put("isEnum", Any.wrap(true));
                                         propMap.put("nameWithEnum", Any.wrap(name.substring(0, 1).toUpperCase() + name.substring(1) + "Enum"));
+                                        this.addUnderscores(entryElement);
                                         propMap.put("value", Any.wrap(entryElement.getValue()));
                                     }
                                     if("format".equals(entryElement.getKey())) {
@@ -484,5 +486,16 @@ public class OpenApiGenerator implements Generator {
             }
         }
         return basePath;
+    }
+
+    private static void addUnderscores(Map.Entry<String, Any> entryElement) {
+        Iterator<Any> iterator = entryElement.getValue().iterator();
+        List<Any> list = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Any any = iterator.next();
+            String value = any.toString().trim().replaceAll(" ", "_");
+            list.add(Any.wrap(value));
+        }
+        entryElement.setValue(Any.wrap(list));
     }
 }
