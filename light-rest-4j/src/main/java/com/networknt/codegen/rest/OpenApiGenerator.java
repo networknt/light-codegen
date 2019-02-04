d6059f1843791d9ac2945d10b1554ac4b389eac8
jiachen1120 <jiachen1120@gmail.com>
2019-02-04 10:40:29 -0500

    - Updated the error message

--- light-rest-4j/src/main/java/com/networknt/codegen/rest/OpenApiGenerator.java
+++ light-rest-4j/src/main/java/com/networknt/codegen/rest/OpenApiGenerator.java
@@ -249,7 +249,7 @@ public class OpenApiGenerator implements Generator {
                     // Since generate a model for primitive types and arrays do not make sense, and an error class would be generated
                     // due to lack of properties if force to generate.
                     if (type == null) {
-                        throw new RuntimeException("Cannot find type of \"" + modelFileName + "\" in #/components/schemas/ of specification file.");
+                        throw new RuntimeException("Cannot find the type of \"" + modelFileName + "\" in #/components/schemas/ of the specification file.");
                     }
                     if (!"object".equals(type)) {
                         continue;
