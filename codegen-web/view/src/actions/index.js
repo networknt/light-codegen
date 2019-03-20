b9d4c23ac8b9cc15a99dd0eb06699e6991a08c51
Steve Hu <stevehu@gmail.com>
2019-03-20 19:04:30 -0400

    add proxy and handler the zip file

--- codegen-web/view/src/actions/index.js
+++ codegen-web/view/src/actions/index.js
@@ -21,8 +21,19 @@ export function submitForm(action) {
         };
         //console.log(request);
         try {
-            const response = await fetch('/api/portal', request);
-            const data = await response.json();
+            const response = await fetch('/api/json', request);
+            const data = await response.blob();
+            const filename = 'light-project.zip';
+            if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                window.navigator.msSaveBlob(data, filename);
+            } else {
+                const url = window.URL.createObjectURL(data);
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+            }
             //console.log("data", data);
             dispatch({ type: SUBMIT_FORM_SUCCESS, payload: data });
             history.push(action.success);
