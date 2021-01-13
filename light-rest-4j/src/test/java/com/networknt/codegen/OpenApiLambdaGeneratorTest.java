a69a93f8953f2923a5d589f6bbcf187afb8d6a6c
Steve Hu <stevehu@gmail.com>
2021-01-12 20:58:29 -0500

    Issue517 (#518)
    * fixes #517 update lambda generator to support light-proxy
    
    * fixes #517 update lambda-proxy with vpc and service templates

--- light-rest-4j/src/test/java/com/networknt/codegen/OpenApiLambdaGeneratorTest.java
+++ light-rest-4j/src/test/java/com/networknt/codegen/OpenApiLambdaGeneratorTest.java
@@ -16,7 +16,9 @@ import java.util.Scanner;
 
 public class OpenApiLambdaGeneratorTest {
     public static String targetPath = "/tmp/openapilambda";
+    public static String proxyTargetPath = "/tmp/openapiProxyLambda";
     public static String configName = "/configlambda.json";
+    public static String configProxyLambda = "/configProxyLambda.json";
     public static String openapiJson = "/openapilambda.json";
     public static String openapiYaml = "/openapilambda.yaml";
 
@@ -45,6 +47,14 @@ public class OpenApiLambdaGeneratorTest {
         String strModel = new Scanner(OpenApiLambdaGeneratorTest.class.getResourceAsStream(openapiYaml), "UTF-8").useDelimiter("\\A").next();
         OpenApiLambdaGenerator generator = new OpenApiLambdaGenerator();
         generator.generate(targetPath, strModel, anyConfig);
+    }
+
+    @Test
+    public void testProxyGeneratorYaml() throws IOException {
+        Any anyConfig = JsonIterator.parse(OpenApiLambdaGeneratorTest.class.getResourceAsStream(configProxyLambda), 1024).readAny();
+        String strModel = new Scanner(OpenApiLambdaGeneratorTest.class.getResourceAsStream(openapiYaml), "UTF-8").useDelimiter("\\A").next();
+        OpenApiLambdaGenerator generator = new OpenApiLambdaGenerator();
+        generator.generate(proxyTargetPath, strModel, anyConfig);
     }
 
     @Test
